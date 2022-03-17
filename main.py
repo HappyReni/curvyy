ade928cdd8e6f3bf9254edbe5e33733e405a58a8
Piljae Won <dnjsvlfwo@gmail.com>
2022-03-17 23:42:49 +0900

    [feat] 삼색 신호 UI 추가

--- main.py
+++ main.py
@@ -11,23 +11,85 @@ class App(QWidget):
 
     def initUI(self):
         # Main Window 정보 설정
-        self.setWindowTitle("손글씨 그리기")
+        self.setWindowTitle("Curvyy")
         self.resize(800, 600)
         ##
 
-        # Canvas
+        # Components
         canvas = Canvas()
+        textbox = QTextEdit()
+        red_color = QColor(255, 96, 92)
+        orange_color = QColor(255, 189, 68)
+        green_color = QColor(0, 202, 78)
+
+        red = Signal(red_color)
+        orange = Signal(orange_color)
+        green = Signal(green_color)
         ##
 
         # layout
-        grid = QGridLayout()
-        self.setLayout(grid)
+        hbox = QHBoxLayout()
+        self.setLayout(hbox)
 
-        grid.addWidget(canvas, 0, 0)
-        grid.addWidget(QLabel('Text'), 0, 1)
+        signalbox = QHBoxLayout()
+        signalbox.addWidget(red)
+        signalbox.addWidget(orange)
+        signalbox.addWidget(green)
+        signalbox.addStretch()
+        signalbox.setSpacing(10)
+
+        rightvbox = QVBoxLayout()
+        rightvbox.addWidget(QLabel('Text'))
+        rightvbox.addWidget(textbox)
+        leftvbox = QVBoxLayout()
+        leftvbox.addLayout(signalbox)
+        leftvbox.addWidget(canvas)
+
+        hbox.addLayout(leftvbox)
+        hbox.addLayout(rightvbox)
         ##
 
         self.show()
+
+class Signal(QWidget):
+    def __init__(self,color):
+        super().__init__()
+
+        # self.setSizePolicy(
+        #     QSizePolicy.MinimumExpanding,
+        #     QSizePolicy.MinimumExpanding
+        # )
+
+        self.color = color
+    
+    def sizeHint(self):
+        return QSize(20,20)
+
+    def setColor(self,color):
+        self.color = color
+        self.update()
+
+    def paintEvent(self, e):
+        # qp = QPainter()
+        # qp.begin(self)
+        # qp.setRenderHint(QPainter.Antialiasing)
+        # qp.setPen(QPen(self.color, 10))
+        # qp.drawRoundedRect(1, 1, 100, 100, 50, 50)
+        # # self.scene.addRect(1, 1, 100, 100,QPen(self.color, 10))
+        # qp.end()
+        painter = QPainter(self)
+
+        painter.setPen(QPen(self.color))
+        brush = QBrush(Qt.SolidPattern)
+        brush.setColor(self.color)
+        brush.setStyle(Qt.SolidPattern)
+        painter.setBrush(brush)
+
+        painter.drawRoundedRect(0, 0, 20, 20, 60, 60)
+        # painter.fillRect(rect, brush)
+        painter.end()
+
+
 
 
 class Canvas(QGraphicsView):
